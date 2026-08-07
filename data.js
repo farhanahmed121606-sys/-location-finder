b49311dba52bdd7cd16da47e0941f7d1894e50a7
farhanahmed121606-sys <farhanahmed121606@gmail.com>
2026-08-07 11:05:45 +0600

    Create data.js

---
+++ data.js
@@ -0,0 +1,70 @@
+const locations = [
+
+  // Dhaka Division
+  { area: "Mirpur", thana: "Mirpur", district: "Dhaka", division: "Dhaka" },
+  { area: "Uttara", thana: "Uttara", district: "Dhaka", division: "Dhaka" },
+  { area: "Dhanmondi", thana: "Dhanmondi", district: "Dhaka", division: "Dhaka" },
+  { area: "Gulshan", thana: "Gulshan", district: "Dhaka", division: "Dhaka" },
+  { area: "Banani", thana: "Gulshan", district: "Dhaka", division: "Dhaka" },
+  { area: "Mohakhali", thana: "Tejgaon", district: "Dhaka", division: "Dhaka" },
+  { area: "Farmgate", thana: "Tejgaon", district: "Dhaka", division: "Dhaka" },
+  { area: "Motijheel", thana: "Motijheel", district: "Dhaka", division: "Dhaka" },
+  { area: "Ramna", thana: "Ramna", district: "Dhaka", division: "Dhaka" },
+  { area: "Badda", thana: "Badda", district: "Dhaka", division: "Dhaka" },
+  { area: "Demra", thana: "Demra", district: "Dhaka", division: "Dhaka" },
+  { area: "Keraniganj", thana: "Keraniganj", district: "Dhaka", division: "Dhaka" },
+  { area: "Savar", thana: "Savar", district: "Dhaka", division: "Dhaka" },
+  { area: "Nawabganj", thana: "Nawabganj", district: "Dhaka", division: "Dhaka" },
+
+  // Gazipur
+  { area: "Tongi", thana: "Tongi", district: "Gazipur", division: "Dhaka" },
+  { area: "Joydebpur", thana: "Gazipur Sadar", district: "Gazipur", division: "Dhaka" },
+  { area: "Kaliakair", thana: "Kaliakair", district: "Gazipur", division: "Dhaka" },
+  { area: "Sreepur", thana: "Sreepur", district: "Gazipur", division: "Dhaka" },
+
+  // Narayanganj
+  { area: "Narayanganj", thana: "Narayanganj Sadar", district: "Narayanganj", division: "Dhaka" },
+  { area: "Fatullah", thana: "Fatullah", district: "Narayanganj", division: "Dhaka" },
+  { area: "Sonargaon", thana: "Sonargaon", district: "Narayanganj", division: "Dhaka" },
+
+  // Khulna Division
+  { area: "Khalishpur", thana: "Khalishpur", district: "Khulna", division: "Khulna" },
+  { area: "Sonadanga", thana: "Sonadanga", district: "Khulna", division: "Khulna" },
+  { area: "Daulatpur", thana: "Daulatpur", district: "Khulna", division: "Khulna" },
+  { area: "Boyra", thana: "Sonadanga", district: "Khulna", division: "Khulna" },
+  { area: "Rupsha", thana: "Rupsha", district: "Khulna", division: "Khulna" },
+
+  // Bagerhat
+  { area: "Bagerhat", thana: "Bagerhat Sadar", district: "Bagerhat", division: "Khulna" },
+  { area: "Mongla", thana: "Mongla", district: "Bagerhat", division: "Khulna" },
+  { area: "Rampal", thana: "Rampal", district: "Bagerhat", division: "Khulna" },
+
+  // Jessore
+  { area: "Jessore", thana: "Jessore Sadar", district: "Jessore", division: "Khulna" },
+  { area: "Benapole", thana: "Sharsha", district: "Jessore", division: "Khulna" },
+  { area: "Sharsha", thana: "Sharsha", district: "Jessore", division: "Khulna" },
+
+  // Chattogram Division
+  { area: "Agrabad", thana: "Double Mooring", district: "Chattogram", division: "Chattogram" },
+  { area: "Pahartali", thana: "Pahartali", district: "Chattogram", division: "Chattogram" },
+  { area: "Halishahar", thana: "Halishahar", district: "Chattogram", division: "Chattogram" },
+  { area: "Kotwali", thana: "Kotwali", district: "Chattogram", division: "Chattogram" },
+
+  // Sylhet
+  { area: "Zindabazar", thana: "Kotwali", district: "Sylhet", division: "Sylhet" },
+  { area: "Beanibazar", thana: "Beanibazar", district: "Sylhet", division: "Sylhet" },
+
+  // Rajshahi
+  { area: "Boalia", thana: "Boalia", district: "Rajshahi", division: "Rajshahi" },
+  { area: "Motihar", thana: "Motihar", district: "Rajshahi", division: "Rajshahi" },
+
+  // Rangpur
+  { area: "Rangpur", thana: "Rangpur Sadar", district: "Rangpur", division: "Rangpur" },
+
+  // Barishal
+  { area: "Barishal", thana: "Kotwali", district: "Barishal", division: "Barishal" },
+
+  // Mymensingh
+  { area: "Mymensingh", thana: "Mymensingh Sadar", district: "Mymensingh", division: "Mymensingh" }
+
+];
